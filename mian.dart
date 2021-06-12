00db565cdb4ad92d3e1be4acbf0054cafcaff146
builder296 <kessirin296@gmail.com>
2021-06-12 23:48:35 +0700

    Operators!

--- mian.dart
+++ mian.dart
@@ -1,5 +1,5 @@
-
-main() { // entry point for execution
+main() {
+  // entry point for execution
 
   //Variable = spot in memory.Or just a bucket to hold value.
 
@@ -18,18 +18,22 @@ main() { // entry point for execution
   * Const and final keywords
   * const = we use this keyword when we want the value/variable to be constant at compile-time
   * final - if we want to variable/value to always be constant = never change
+
+    Operators = Arithmetic ( -, +, *, /, % (remainder) = modulo
+    Equality and Relational Operators ( ==, !=, >, <, >=, <=)
+    as, is and is!
   *
   * */
 
-  var country = "Canada";
-  const pi = 3.14;
+  var result = 4 / 2;
+
+  // print(pi <= gravity);
+
+  print(result is! bool);
+
+  // var country = "Canada";
+  // const pi = 3.14;
   // pi = 29.34;
-
-
-
-
-
-
 
   // bool isTrue = true;
   // bool isFalse = false;
@@ -37,19 +41,10 @@ main() { // entry point for execution
   // print(isTrue);
   // print(isFalse);
 
-  
-  
-  
-  
-  
   // num age = 27;
   // double number = 29.99;
   //
   // print(number);
-
-
-
-
 
   // var country;
   //
