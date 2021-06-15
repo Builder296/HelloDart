327688d1ce7afcd244ab83f960e19bf2357919e6
builder296 <kessirin296@gmail.com>
2021-06-15 19:46:02 +0700

    Try logic

--- main.dart
+++ main.dart
@@ -18,18 +18,51 @@ main() {
   * Const and final keywords
   * const = we use this keyword when we want the value/variable to be constant at compile-time
   * final - if we want to variable/value to always be constant = never change
-
-    Operators = Arithmetic ( -, +, *, /, % (remainder) = modulo
-    Equality and Relational Operators ( ==, !=, >, <, >=, <=)
-    as, is and is!
   *
   * */
 
-  var result = 4 / 2;
+  // xooooox 5   1 1
+  // xxoooox 4   2 1
+  // xoxooox 3 1 1 1 1
+  // xooxoox 2 2 1 1 1
+  // xoooxox 1 3 1 1 1
+  // xooooxx   4 1 2
+  // xooooox   5 1 1
 
-  // print(pi <= gravity);
+  // xooox 1     3 1
+  // xxoox 2     2 1
+  // xoxox 1 1 1 1 1
+  // xooxx 1 2     2
+  // xooox 1 3     1
+  const y = 5;
 
-  print(result is! bool);
+  for (int a = 1; a <= y; a++) {
+    // row
+    var row = "";
+    var maxO = y - 2;
+    var startO = maxO + 1;
+    for (int b = 1; b <= y; b++) {
+      // column
+      if (b == 1 || b == y) {
+        row = row + "x";
+      } else {
+        if (b <= startO && (a == 1 || a == y)) {
+          row = row + "o";
+        } else {
+          if (b <= (startO - 1) && (a == y - 1)) {
+            row = row + "o";
+          } else {
+            if (b <= (startO - 2) && (a == y - 2)) {
+              row = row + "o";
+            } else {
+              row = row + "x";
+            }
+          }
+        }
+      }
+    }
+    print(row);
+  }
 
   // var country = "Canada";
   // const pi = 3.14;
